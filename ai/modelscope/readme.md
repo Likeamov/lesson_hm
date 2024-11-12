5c94d7fd56e496c51182766a0f1e20c2bf6cc40c
Likeamov <jinzitao2004@163.com>
2024-11-12 21:30:28 +0800

    LLM

---
+++ ai/modelscope/readme.md
@@ -0,0 +1,6 @@
+ # 魔搭社区
+ > 国内最大AI模型社区
+
+ ## 大模型 LLM  Large(参数规模) Language Model
+ - 输出 -> code(编程) ->输出
+ 
--- js/scope/5.js
+++ js/scope/5.js
@@ -1,6 +1,9 @@
-function foo(){
-    console.log(a+b)
+// Reference：b is not defined
+//RHS 查找 失败的时候 ，会爆出ReferenceError 错误，报错
+function foo(a){
+    console.log(a + b)
     b = a;
 }
 
 foo(2);
+  console.log(b);
