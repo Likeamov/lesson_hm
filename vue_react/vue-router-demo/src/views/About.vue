b61e6a913519324570fe49fc395b73e25be58b64
Likeamov <jinzitao2004@163.com>
2025-01-04 17:03:39 +0800

    post-meta

--- vue_react/vue-router-demo/src/views/About.vue
+++ vue_react/vue-router-demo/src/views/About.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-     About
+      About
+      <button @click="goHome">回到首页</button>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script setup>
+  import  {useRouter} from 'vue-router'
+  const router = useRouter() // 路由总管
+  
+  const goHome = () => {
+    // js 跳转  
+    // Browser Object Model > DOM   
+    //BOM API 
+    window.location.href="/"
+    router.push('/')  
+}
+  </script>
+  
+  <style  scoped>
+  
+  </style>
+  
