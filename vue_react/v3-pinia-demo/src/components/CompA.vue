20f00f68d37cf07b88808b42b7df7cc4f05b3092
Likeamov <jinzitao2004@163.com>
2025-01-11 00:08:27 +0800

    v3

---
+++ vue_react/v3-pinia-demo/src/components/CompA.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h2>CompA</h2>
+      <p>count: {{ count }}</p>
+      <button @click="increment">Add</button>
+      <!-- <CompSubA :count="props.count"/> -->
+    </div>
+  </template>
+  
+  <script setup>
+  // 引入中央模块
+  import { useCounterStore } from '../store/counter'; 
+  const counterStore = useCounterStore();
+  console.log(counterStore)
+  const { count, increment } = counterStore;
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
---
+++ vue_react/v3-pinia-demo/src/components/CompB.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h2>CompB</h2>
+      <p>count: {{ props.count }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
