d17a4c4a60313dd2276a1ba741322d6736108959
Likeamov <jinzitao2004@163.com>
2025-02-09 01:10:43 +0800

    deepseek

---
+++ deepseek/deepseek-chatbot/server/index.js
@@ -0,0 +1,34 @@
+// commonjs
+// node 最简单的后端框架
+const koa = require('koa');
+// 实例化
+const app = new koa(); // 也是应用
+const Router = require('koa-router'); // 路由
+const router = new Router(); // 实例化
+// 新建 / 路由
+router.get('/', async (ctx) => {
+  // 响应体
+  // ctx 这一次请求响应的上下文
+  ctx.body = `
+    <html>
+      <head>
+        <title>chatbot</title>
+      </head>
+      <body>
+        <h1>chatbot</h1>
+      </body>
+    </html>
+  `;
+})
+
+// 新建一个叫做 chatai 的路由 
+//  method, url 
+router.post("/chatai", async (ctx) => {
+
+})
+// 路由的挂载app
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log("server is running at port 3000");
+})
