ca4ad9de194d92e434895c69af3b4a5fbcfe85b7
Likeamov <jinzitao2004@163.com>
2025-03-17 21:36:00 +0800

    V3

--- interview/chunzhao/vue/watch_effect/v3-shop-cart/src/store/cart.ts
+++ interview/chunzhao/vue/watch_effect/v3-shop-cart/src/store/cart.ts
@@ -1,6 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import { useProductStore } from './product'
+import { useProductsStore } from './product'
 
 type CartItem = {
   productId: number;
@@ -12,23 +12,36 @@ export const useCartStore = defineStore('cart', () => {
   // vue3 vue2 区别？ 
   // vue3 全面支持 ts  99% 代码ts写的
   const items = ref<CartItem[]>([])
-  const productStore = useProductStore()
+  const productStore = useProductsStore()
   // items.value.push(1)
-  // 使用computed计算属性监听items的变化
-  // 当items数组发生变化时(添加、删除、修改数量)，会自动重新计算totalPrice
   const totalPrice = computed(() => {
     // 累加和
     return items.value.reduce((total, item) => {
-      // 根据商品ID在商品列表中查找对应商品
+      // null
       const product = productStore.products.find(p => p.id === item.productId)
-      // 如果找到商品,计算该商品的总价(单价 * 数量)并加到总计中
-      // 如果未找到商品,加 0 到总计中
+      // ES6 中的可选链操作符
       return total + (product ? product.price * item.quantity : 0)
-    }, 0) // 初始值为0
+    }, 0)
   })
+
+  const getQuantity = (productId: number) => {
+    const item = items.value.find(item => item.productId === productId)
+    return item ? item.quantity : 0
+  }
+
+  const addToCart = (productId: number) => {
+    const existingItem = items.value.find(item => item.productId === productId)
+    if (existingItem) {
+      existingItem.quantity++
+    } else {
+      items.value.push({ productId, quantity: 1 })
+    }
+  }
 
   return {
     items,
-    totalPrice
+    totalPrice,
+    getQuantity,
+    addToCart
   }
 })
--- interview/chunzhao/vue/watch_effect/v3-shop-cart/src/store/product.ts
+++ interview/chunzhao/vue/watch_effect/v3-shop-cart/src/store/product.ts
@@ -1,6 +1,6 @@
-import {ref} from 'vue'
+import { ref, onMounted } from 'vue'
 import { defineStore } from 'pinia'
-import { getProducts } from '../api/product'
+import { getProducts } from '../api/product';
 
 export interface Product {
   id: number;
@@ -9,20 +9,17 @@ export interface Product {
   inventory: number;
 }
 
-export const useProductStore = defineStore('product', () => {
-  // 使用 ref 创建一个响应式数组,用于存储商品数据
-  // 数组的类型是 Product[] (Product 类型的数组)
-  // 初始值为空数组 []
-  const products =  ref([] as Product[])// 断言
+export const useProductsStore = defineStore('product', () => {
+  const products = ref([] as Product[]) // 断言
   
-  // 定义一个异步函数,用于加载商品数据
   const loadProducts = async () => {
-    // 使用 getProducts 函数从 API 获取商品数据
-    // 这里假设 getProducts 是一个异步函数,返回一个 Promise<Product[]>
-    const result = await getProducts()
-    products.value = result
+    products.value = await getProducts()
   }
-  
+
+  onMounted(() => {
+    loadProducts()
+  })
+
   return {
     products,
     loadProducts
